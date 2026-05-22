aa014bc6df44d3923fc9a96dd642773e49cd0fd1
Mitchi Eala <64bck7sszn@privaterelay.appleid.com>
2026-05-23 07:23:50 +0900

    feat: scaffold api package

---
+++ api/src/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => console.log(`API listening on port ${PORT}`));
+
+module.exports = app;
